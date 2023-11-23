eb3686e44c8de40a5513c128ccf818fb771028c9
gabriel lima <gabriellima140806@gmail.com>
2023-11-23 07:55:12 -0300

    continuar depoi

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const path = require("path")
 
 const app = express()
 
-const caminhoBase = path.join(__dirname, "templates")
+const caminhoBase = path.join(__dirname, "templantes")
+
+app.post('/cadstrar/salvar', (requisiçao, respotas)=>)
 
 app.get('/cadastrar',(requisiçao, resposta) => {
-    resposta.sendFile(`${caminhaBase}/cadastro.html`)
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
 })
 app.get('/usuario/:id ',(requisiçao, resposta)=>{
 const id = requisiçao.params.id
 console.log(id)
-resposta.sendFile( `${caminhoBase}/usuarios.hmtl`)
+resposta.sendFile(`${caminhoBase}/usuarios.html`)
 })
 
-app.get('/' , (requisiçao, resposta)=>{
-    resposta.sendFile(` $ {caminhoBase} /index.html` )
+app.get('/', (requisiçao, resposta)=>{
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, ()=>{
